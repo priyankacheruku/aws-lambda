a1739fabd23b03faa0875962fbeb42a7fb3f70a4
Priyanka <priyanka.cheruku@phenompeople.com>
2021-09-27 15:50:53 +0530

    dockerfile and python code

---
+++ app.py
@@ -0,0 +1,7 @@
+import json
+
+def handler(event, context):
+    # the handler will accept put/post calls
+    # `event` consists of payload
+    
+    return 'Hello Priyanka ! from AWS Lambda using Python'
